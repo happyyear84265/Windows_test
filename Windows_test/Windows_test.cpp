034c6635391190400a24720ecd089f4ba7f018bc
個人練習用程式 <59037243+happyyear84265@users.noreply.github.com>
2020-09-07 13:17:36 +0800

    Add files via upload

---
+++ Windows_test/Windows_test.cpp
@@ -0,0 +1,225 @@
+﻿// Windows_test.cpp : 定義應用程式的進入點。
+//
+
+#include "framework.h"
+#include "Windows_test.h"
+
+#define MAX_LOADSTRING 100
+
+// 全域變數:
+HINSTANCE	hInst;                                // 目前執行個體
+WCHAR		szTitle[MAX_LOADSTRING];                  // 標題列文字
+WCHAR		szWindowClass[MAX_LOADSTRING];            // 主視窗類別名稱
+RECT		WindowRect;
+BOOL		joker = false;
+// 這個程式碼模組所包含之函式的向前宣告:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// TODO: 在此放置程式碼。
+
+	// 將全域字串初始化
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_WINDOWSTEST, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// 執行應用程式初始化:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSTEST));
+	MSG msg;
+
+	// 主訊息迴圈:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+//
+//  函式: MyRegisterClass()
+//
+//  用途: 註冊視窗類別。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSTEST));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_WINDOWSTEST);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassExW(&wcex);
+}
+
+//
+//   函式: InitInstance(HINSTANCE, int)
+//
+//   用途: 儲存執行個體控制代碼並且建立主視窗
+//
+//   註解:
+//
+//        在這個函式中，我們將執行個體控制代碼儲存在全域變數中，
+//        並建立及顯示主程式視窗。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	hInst = hInstance; // 將執行個體控制代碼儲存在全域變數中
+
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
+//
+//  函式: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  用途: 處理主視窗的訊息。
+//
+//  WM_COMMAND  - 處理應用程式功能表
+//  WM_PAINT    - 繪製主視窗
+//  WM_DESTROY  - 張貼結束訊息然後傳回
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// 剖析功能表選取項目:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_WAIT:
+		{
+			int times = 0;
+			while (true)
+			{
+				MessageBox(0, L"跟你說別按了", L"Warning", 0);
+				times++;
+				if (times > 5)
+				{
+					MessageBox(0, L"記住 別手癢", L"Warning", 0);
+					joker = true;
+					InvalidateRect(hWnd, NULL, true);
+					UpdateWindow(hWnd);
+					break;
+				}
+			}
+		}
+		break;
+		case IDM_EXIT:
+		{
+			int msgboxID = MessageBox(0, L"你確定你要離開?", L"確認", MB_OKCANCEL);
+			if (msgboxID == IDOK)
+			{
+				int msgboxID1 = MessageBox(0, L"好吧記得要反著按?", L"確認", MB_OKCANCEL);
+				if (msgboxID1 == IDOK)
+				{
+					MessageBox(0, L"就跟你說要反著按了 重來吧", L"嘲笑ㄎ", 0);
+				}
+				else if (msgboxID1 == IDCANCEL)
+				{
+					MessageBox(0, L"好吧 掰掰", L"See you again", 0);
+					DestroyWindow(hWnd);
+				}
+			}
+		}
+		break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+	case WM_PAINT:
+	{
+		GetWindowRect(hWnd, &WindowRect);
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// TODO: 在此新增任何使用 hdc 的繪圖程式碼...
+		for (int x = 0; x < WindowRect.right - 200; x = x + 200)
+		{
+			for (int y = 0; y < WindowRect.bottom - 200; y = y + 200)
+			{
+				if (joker == false)
+					TextOut(hdc, x, y, L"Hello World!", lstrlen(L"Hello World"));
+				else
+				{
+					TextOut(hdc, x, y, L"GoodBye World", lstrlen(L"GoodBye World"));
+					TextOut(hdc, x, y+50, L"手癢得治", lstrlen(L"手癢得治"));
+				}
+			}
+		}
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// [關於] 方塊的訊息處理常式。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
